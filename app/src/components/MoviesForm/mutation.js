0d0c79dae5b4e66d519bda1621a4cd51f5953db9
Mikhail <misha1247@mail.ru>
2019-12-08 23:41:31 +0300

    Add elements to DB

---
+++ app/src/components/MoviesForm/mutation.js
@@ -0,0 +1,15 @@
+import { gql } from 'apollo-boost';
+
+export const addDMovieMutation = gql`
+  mutation addMovie($name: String!, $genre: String!, $rate: Int!, $watched: Boolean, $directorId: ID) {
+    addMovie(name: $name, genre: $genre, rate: $rate, watched: $watched, directorId: $directorId) {
+      name
+      genre
+      watched
+      rate
+      director {
+        name
+      }
+    }
+  }
+`;
